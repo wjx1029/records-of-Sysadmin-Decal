710c6d9e2b14e9ff8f23b9e85e6f4a5d43a2ef70
sean <wanjx0701@gmail.com>
2026-04-03 08:49:15 +0800

    finished lab02

---
+++ lab02/phonebook_skeleton.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+PHONEBOOK_ENTRIES="bash_phonebook_entries"
+
+
+if [ "$#" -lt 1 ]; then
+    exit 1
+
+elif [ "$1" = "new" ]; then
+    # YOUR CODE HERE #
+    echo "$2 $3" >> $PHONEBOOK_ENTRIES
+
+elif [ "$1" = "list" ]; then
+    if [ ! -e $PHONEBOOK_ENTRIES ] || [ ! -s $PHONEBOOK_ENTRIES ]; then
+        echo "phonebook is empty"
+    else
+        # YOUR CODE HERE #
+	cat "$PHONEBOOK_ENTRIES"
+    fi
+
+elif [ "$1" = "lookup" ]; then
+    # YOUR CODE HERE #
+    grep -w "$2" "$PHONEBOOK_ENTRIES"
+
+elif [ "$1" = "remove" ]; then
+    # YOUR CODE HERE #
+    sed -i "/^$2 /d" "$PHONEBOOK_ENTRIES"
+
+elif [ "$1" = "clear" ]; then
+    # YOUR CODE HERE #
+    > $PHONEBOOK_ENTRIES
+
+else
+     # YOUR CODE HERE #
+     echo 'Currently not support this command'
+fi
